37b54070a9088eecfc41513731f8b51fc3652eda
Santiago Montoya <smonto67@gmail.com>
2022-06-13 20:41:58 -0500

    feat: creates the pages index and cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Index";
+import Cart from "./pages/Cart";
+
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route exact path="/">
-            <Home />
-          </Route>
+            <Route path="/" element={<Home/>}>
+              <Route path="/Cart" element={<Cart/>}>
+                </Route>
+            </Route>
         </Routes>
       </Router>
      
--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Index = () => {
   return (
     <div>
-        <h1>Home</h1>
+        <h1>Index</h1>
     </div>
   )
 }
 
-export default Home;
+export default Index;
