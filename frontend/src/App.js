56b5fde72cc2aa9d7583c36d86b1581c2fd8259f
Santiago Montoya <smonto67@gmail.com>
2022-06-14 20:48:15 -0500

    feat: creates categories and products pages and configures fontawesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Index";
 import Cart from "./pages/Cart";
 import Layout from './layout/Layout';
+import Products from './pages/Products';
+import Categories from './pages/Categories';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route path="" element={<Home />}/>
             <Route path="cart" element={<Cart />}/>
+            <Route path="products" element={<Products />}/>
+            <Route path="categories" element={<Categories />}/>
           </Route>
         </Routes>
       </Router>
--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Index = () => {
   return (
     <div>
-        <h1>Index</h1>
+        <h1><i class="fa-solid fa-recycle"></i>Index</h1>
     </div>
   )
 }
