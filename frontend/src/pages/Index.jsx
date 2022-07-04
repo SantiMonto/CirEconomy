caaf863b2c77b4fe78b16c4f5fadae19c8dfb546
Santiago Montoya <smonto67@gmail.com>
2022-07-04 16:44:01 -0500

    feat: modifying the page design with carousel for categories and card for the image

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,101 +1,70 @@
 import React from 'react'
-import { Container, CardGroup, Card} from 'react-bootstrap';
+import { Container, Carousel, Card } from 'react-bootstrap';
 import inicio from '../images/inicio.jpg'
 
 const Index = () => {
   return (
-    <div className='mt-4'>
-      <img src={inicio} className="d-block mx-auto" alt="Inicio" />
-      <hr />
-      <Container>
-        <a href="/categories" class="link-dark">
+    <div>
+      <section className="mt-4">
+        {/* <img src={inicio} className="d-block mx-auto" alt="Inicio" /> */}
+        <Card className="d-block mx-auto" style={{ width: '75rem' }} >
+          <Card.Img img src={inicio} className="d-block mx-auto" alt="Inicio" />
+          <Card.Body className='bg-dark'>
+            <Card.Text className='text-white text-justify'>
+              <h4>
+              CirEconomy es una pagina pensada para aportar al medio ambiente, esto mediante
+              el uso de la economia circular, la cual para el caso particular busca que
+              aquellas empresas luego de terminar un proyecto de construcción, remodelación, o
+              en el cual tengan que utilizar materiales nuevos o maquinaria pesada, los pueda vender
+              mediante una subasta al mejor postor, por lo que varias empresas o particulares pueden
+              ver los productos que se están subastando y enviar una oferta por estos.
+              </h4>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </section>
+      <hr/>
+      <Container className="mt-4">
+        <a href="/categories" className="link-dark">
           <h2>Categorias</h2>
         </a>
-        <CardGroup>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to additional
-                content.{' '}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This card has even longer content than the first to
-                show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-        </CardGroup>
-        <CardGroup>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to additional
-                content.{' '}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This card has even longer content than the first to
-                show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-        </CardGroup>
+        <Carousel className='mt-5' variant="dark">
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=f5f5f5"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=eee"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h5>Second slide label</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h5>Third slide label</h5>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </Container>
+
 
     </div>
   )
