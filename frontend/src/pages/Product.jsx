601414711e69668a76453b8c214a7e1a859f4160
Santiago Montoya <smonto67@gmail.com>
2022-07-04 20:18:41 -0500

    feat: aplying design to the product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Card, Button, Container } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 import Producto from '../images/producto.png'
 
 const Product = () => {
     return (
-        <Container className='mt-4'>
-            <Card style={{ width: '15rem' }}>
-                <Card.Img variant="top" src={Producto} />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
+        <Container className='mt-4 d-flex mx-auto'>
+            <Card>
+                <Card.Header className='bg-dark text-white' as="h4">Nombre del producto</Card.Header>
+                <Row className='my-2 d-flex mx-auto'>
+                    <Col md="auto">
+                        <Card.Img style={{ width: '15rem' }} variant="top" src={Producto} />
+                    </Col>
+                    <Col>
+                        <Card.Img className='my-1' style={{ width: '4.5rem' }} variant="top" src={Producto} />
+                        <Card.Img className='my-1' style={{ width: '4.55rem' }} variant="top" src={Producto} />
+                        <Card.Img className='my-1' style={{ width: '4.55rem' }} variant="top" src={Producto} />
+                    </Col>
+                    <Col md="auto">
+                        <Card.Body>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Col>
+                </Row>
+                <Card.Footer className='text-end bg-dark text-white'>
+                    <Button className='mx-1' variant="light">Ofertar</Button>
+                    <Button className='mx-1' variant="light">Regresar</Button>
+                </Card.Footer>
             </Card>
         </Container>
     )
