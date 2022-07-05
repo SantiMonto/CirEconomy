50ec89628e97507ad17e5a12913906afac19fcb9
Santiago Montoya <smonto67@gmail.com>
2022-07-04 21:11:46 -0500

    feat: completed the produt page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, Container, Row, Col } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col, Form, InputGroup } from 'react-bootstrap';
 import Producto from '../images/producto.png'
 
 const Product = () => {
@@ -7,26 +7,42 @@ const Product = () => {
         <Container className='mt-4 d-flex mx-auto'>
             <Card>
                 <Card.Header className='bg-dark text-white' as="h4">Nombre del producto</Card.Header>
-                <Row className='my-2 d-flex mx-auto'>
-                    <Col md="auto">
+                <Row className='my-2 mx-2'>
+                    <Col xs={3} className='mx-2'>
                         <Card.Img style={{ width: '15rem' }} variant="top" src={Producto} />
                     </Col>
-                    <Col>
+                    <Col xs={1}>
                         <Card.Img className='my-1' style={{ width: '4.5rem' }} variant="top" src={Producto} />
                         <Card.Img className='my-1' style={{ width: '4.55rem' }} variant="top" src={Producto} />
                         <Card.Img className='my-1' style={{ width: '4.55rem' }} variant="top" src={Producto} />
                     </Col>
-                    <Col md="auto">
+                    <Col xs={6}>
                         <Card.Body>
                             <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
+                                <p>
+                                    <h3>Nombre del producto</h3>
+                                </p>
+                                <p>
+                                    <h6>
+                                        Descripcion del producto, donde se den detalles tecnicos y
+                                        se garantice la calidad del producto sobrante a vender
+                                    </h6>
+                                </p>
+                                <section className='my-1'>
+                                    <h6>Este es el valor en COP actual de la subasta</h6>
+                                    <input placeholder="Valor inicial de la subasta" readOnly />
+                                </section>
+                                <section className='my-1'>
+                                    <h6>Ingrese el valor que desea ofertar en COP</h6>
+                                    <input className="mb-3" placeholder="Valor a ofertar" defaultValue={'$'}>
+                                    </input>
+                                </section>
                             </Card.Text>
                         </Card.Body>
                     </Col>
                 </Row>
-                <Card.Footer className='text-end bg-dark text-white'>
-                    <Button className='mx-1' variant="light">Ofertar</Button>
+                <Card.Footer className='text-end bg-white text-white'>
+                    <Button className='mx-1' variant="dark">Ofertar</Button>
                     <Button className='mx-1' variant="light">Regresar</Button>
                 </Card.Footer>
             </Card>
