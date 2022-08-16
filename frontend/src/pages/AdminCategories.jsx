bd2c739d44e064f2829ead3e6e09298b770738bd
Santiago Montoya <smonto67@gmail.com>
2022-08-15 22:26:06 -0500

    feat(frontend): added fontawesome and modifies the edit categories with the edit button

--- frontend/src/pages/AdminCategories.jsx
+++ frontend/src/pages/AdminCategories.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { Accordion, Container, Table, Form, Button } from 'react-bootstrap'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AdminCategories = () => {
     const [nameCategorie, setNameCategorie] = useState([]);
-    const [imgCategorie,setImgCategorie] = useState([]);
+    const [imgCategorie, setImgCategorie] = useState([]);
 
-    const createCategorie = async() => {
-        console.log("nombre",nameCategorie,"imagen",imgCategorie);
+    const createCategorie = async () => {
+        console.log("nombre", nameCategorie, "imagen", imgCategorie);
+        toast.success('Categoria creada con exito');
+    }
+
+    const editCategorie = async () => {
+        console.log('Edita la categoria seleccionada');
     }
 
     return (
@@ -19,57 +26,70 @@ const AdminCategories = () => {
                             <Form>
                                 <Form.Group className="mb-3" controlId="Name">
                                     <Form.Label>Nombre de la categoria</Form.Label>
-                                    <Form.Control 
-                                    type="name" 
-                                    placeholder="Ingrese el nombre de la nueva categoria" 
-                                    value = {nameCategorie}
-                                    onChange ={ (e) =>{
-                                        setNameCategorie(e.target.value);
-                                    }}
+                                    <Form.Control
+                                        type="name"
+                                        placeholder="Ingrese el nombre de la nueva categoria"
+                                        value={nameCategorie}
+                                        onChange={(e) => {
+                                            setNameCategorie(e.target.value);
+                                        }}
                                     />
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="URL">
                                     <Form.Label>URL de la imagen de la categoria</Form.Label>
-                                    <Form.Control 
-                                    type="img" 
-                                    placeholder="Ingrese la URL de la imagen de la categoria" 
-                                    value = {imgCategorie}
-                                    onChange ={(e)=>{
-                                        setImgCategorie(e.target.value);
-                                    }}
+                                    <Form.Control
+                                        type="img"
+                                        placeholder="Ingrese la URL de la imagen de la categoria"
+                                        value={imgCategorie}
+                                        onChange={(e) => {
+                                            setImgCategorie(e.target.value);
+                                        }}
                                     />
                                 </Form.Group>
-                                <Button 
-                                variant="dark" 
-                                type="button" 
-                                onClick={() => {
-                                    createCategorie();
-                                }}
+                                <Button
+                                    variant="dark"
+                                    type="button"
+                                    onClick={() => {
+                                        createCategorie();
+                                    }}
                                 >
                                     Agregar
                                 </Button>
+                                <ToastContainer
+                                    position="top-center"
+                                    autoClose={5000}
+                                />
                             </Form>
                         </Accordion.Body>
                     </Accordion.Item>
                     <Accordion.Item eventKey="1">
                         <Accordion.Header>Modificar categoria</Accordion.Header>
                         <Accordion.Body>
-                            <Table striped bordered hover variant='dark'>
+                            <Table striped bordered hover>
                                 <thead>
                                     <tr>
                                         <th>#</th>
                                         <th>Nombre</th>
                                         <th>URL de la imagen</th>
-                                        <th>Acción</th>
+                                        <th>Editar</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
                                         <td>1</td>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
+                                        <td>Residuos de arena</td>
+                                        <td>www.imagenesweb.con/residuos</td>
+                                        <td>
+                                            <Button
+                                                type="button"
+                                                onClick={() => {
+                                                    editCategorie();
+                                                }}
+                                            >
+                                                <i className="fa-solid fa-pen-to-square"></i>
+                                            </Button>
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td>2</td>
