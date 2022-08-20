687f31a662f4a7b10105d896e06ba780a236d9d1
Santiago Montoya <smonto67@gmail.com>
2022-08-20 12:47:35 -0500

    feat(frontend): applying margin top to the admin tabs, categories is the default tab in admin

--- frontend/src/layout/AdminLayout.jsx
+++ frontend/src/layout/AdminLayout.jsx
@@ -5,13 +5,13 @@ import { Outlet } from 'react-router-dom';
 const AdminLayout = () => {
     return (
         <div>
-            <Container>
-                <Nav fill variant="tabs" defaultActiveKey="/AdminLayout">
+            <Container className="mt-2">
+                <Nav fill variant="tabs">
                     <Nav.Item>
-                        <Nav.Link className="link-dark" href="/adminLayout/adminCategories">Categorias</Nav.Link>
+                        <Nav.Link className="link-dark border border-dark" href="/adminLayout/adminCategories">Categorias</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link className="link-dark" href="/adminLayout/adminProducts">Productos</Nav.Link>
+                        <Nav.Link className="link-dark border border-dark" href="/adminLayout/adminProducts">Productos</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </Container>
